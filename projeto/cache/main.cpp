f65f28977b80398d215e4883b1ebf30f31e71a9b
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-10-14 21:40:32 -0300

    Add files via upload

---
+++ projeto/cache/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+
+#include <my-lib/bit.h>
+
+using BitSet = Mylib::BitSet<32>;
+
+int main (int argc, char **argv)
+{
+	std::cout<<"salve"<<std::endl;
+
+	return 0;
+}
+
+int checaMemoria
