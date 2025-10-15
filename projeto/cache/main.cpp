9df0f4f02bec273e02684cb29bb2a3fab72dfe98
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-10-14 22:23:59 -0300

    Add files via upload

--- projeto/cache/main.cpp
+++ projeto/cache/main.cpp
@@ -3,13 +3,30 @@
 
 #include <my-lib/bit.h>
 
+#include <vector>
+
 using BitSet = Mylib::BitSet<32>;
 
 int main (int argc, char **argv)
 {
-	std::cout<<"salve"<<std::endl;
+	std::vector<BitSet> memoria(5);
+
+	memoria[3] = 2;
+
+	std::cout<<checaMemoria(2)<<std::endl;
 
 	return 0;
 }
 
-int checaMemoria
+bool checaMemoria(BitSet dado, std::vector<BitSet> memoria)
+{
+	bool esta = false;
+
+	for(int i = 0; i < 5; i++) {
+		if(memoria[i] == dado) {
+			esta = true;
+		}
+	}
+
+	return esta;
+}
---
+++ projeto/cache/memoria.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdint>
+
+#include <my-lib/bit.h>
+
+#include <vector>
+
+using BitSet = Mylib::BitSet<32>;
+
